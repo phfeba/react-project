a52741d458d37b7f02ebeb388aec2d2bf92316f9
phfeba <phfeba@hotmail.com>
2024-05-15 22:24:57 -0300

    weather card component added

--- clima/src/App.js
+++ clima/src/App.js
@@ -1,7 +1,42 @@
+import { useState } from 'react';
 import './App.css';
+import TempCard from './components/temp-card';
 
 function App() {
-  return <div className='App'></div>;
+  const diasSemana = [
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado',
+  ];
+  let [contadorState, setContadorState] = useState(0);
+  let contador = 0;
+  return (
+    <div className='App'>
+      <button
+        onClick={() => {
+          setContadorState((contadorState += 1));
+        }}
+      >
+        Clique aqui 1
+      </button>
+      <h1>{contadorState}</h1>
+      <button
+        onClick={() => {
+          console.log((contador += 1));
+        }}
+      >
+        Clique aqui 2
+      </button>
+      <h1>{contador}</h1>
+      {diasSemana.map((dia) => {
+        return <TempCard diaDaSemana={dia} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
