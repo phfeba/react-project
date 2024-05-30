8c3b6b88310591798bd781b3d114c18644830401
phfeba <phfeba@hotmail.com>
2024-05-29 21:48:30 -0300

    temperature card added

--- clima/src/App.js
+++ clima/src/App.js
@@ -1,9 +1,10 @@
 import { useRef, useState } from 'react';
 import './App.css';
+import TempCardP from './components/temp-card-princ';
 
 function App() {
-  // const diasSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
-  // let [contadorState, setContadorState] = useState(0);
+  const diasSemana = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'];
+  const diasAMostrar = [0, 1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5, 6];
   const [tempState, setTempState] = useState();
   const [cityState, setCityState] = useState();
   const [city, setCity] = useState('');
@@ -17,12 +18,28 @@ function App() {
         return res.json();
       })
       .then((dadoTemp) => {
+        console.log(dadoTemp);
         setDescricao(dadoTemp.weather[0].description);
         setTempState(dadoTemp.main.temp);
         setCity(dadoTemp.name);
       });
     iValue.current.value = '';
+    let urlDois = `https://api.openweathermap.org/data/2.5/forecast?q=${cityState}&lang=pt_br&units=metric&appid=109f862a42ad10e6005a6fc30750eca5`;
+    fetch(urlDois)
+      .then((res) => {
+        return res.json();
+      })
+      .then((dadoTemp) => {
+        console.log(dadoTemp);
+        // diasSemana.map((dia) => {});
+        for (let i = hoje; i < hoje + 6; i++) {
+          console.log(diasSemana[diasAMostrar[i + 1]]);
+        }
+      });
   };
+
+  let today = new Date();
+  let hoje = today.getUTCDay();
 
   return (
     <div className='App'>
@@ -33,20 +50,21 @@ function App() {
       ></input>
       <button onClick={callApi}>buscar</button>
 
-      <p>
-        {city ? `${city}: ` : ''} {tempState}
-      </p>
-      <p>{descricao}</p>
-      {/* <button
-        onClick={() => {
-          setContadorState((contadorState += 1));
-        }}
-      >
-        Clique aqui 1
-      </button>
-      <h1>{contadorState}</h1> */}
+      <TempCardP
+        diaDaSemana={city ? diasSemana[today.getUTCDay()] : ''}
+        cityName={city ? city : ''}
+        temp={tempState}
+        description={descricao}
+      />
+
+      {}
+
       {/* {diasSemana.map((dia) => {
-        return <TempCard diaDaSemana={dia} />;
+        return (
+          <div>
+            <p>{dia}</p>
+          </div>
+        );
       })} */}
     </div>
   );
---
+++ clima/src/components/temp-card-sec/indes.js
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function TempCardS({ diaDaSemana, cityName, temp, description }) {
+  return (
+    <div className='diaTemp'>
+      <h2>{cityName}</h2>
+      <h3>{diaDaSemana}</h3>
+      <h1>{temp}</h1>
+      <p>{description}</p>
+    </div>
+  );
+}
